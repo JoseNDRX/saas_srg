4d1349aeb3f1123ccd5db138a303c8074c4ebc37
JoseNDRX <jandrexrmz@yahoo.com>
2026-03-29 15:31:17 -0600

    vercel

--- proxy.ts
+++ proxy.ts
@@ -34,7 +34,7 @@ export function proxy(request: NextRequest) {
     hostWithoutPort.startsWith('192.168.') ||
     hostWithoutPort.includes('ngrok-free.dev') ||
     hostWithoutPort.includes('ngrok-free.app') ||
-    hostWithoutPort.includes('saassrg2026.vercel.app/');
+    hostWithoutPort.includes('vercel.app/');
 
   const isStandMXBase = hostWithoutPort === 'standmx.com' || hostWithoutPort === 'www.standmx.com';
   const parts = hostWithoutPort.split('.');
